0ce62aa5dee7aae68510934d5f45b81eae61f459
Monika Wahi <dethwench@gmail.com>
2022-10-12 09:37:24 -0400

    Add files via upload

---
+++ How to Do Data Input Output and Paste in R/115_Basic Paste.R
@@ -0,0 +1,7 @@
+#phone number with nothing in sep
+PhoneNumber <- paste(612,781,8888,sep="")
+class(PhoneNumber)
+
+#phone number with characters and dash in sep
+PhoneNumberWithDashes <- paste(612,781,"TVTV",sep="-")
+
---
+++ How to Do Data Input Output and Paste in R/120_Complex paste
@@ -0,0 +1,7 @@
+lineitems <- readRDS("lineitems.rds")
+
+#maximum total cost
+max(lineitems$Tot_Cost)
+
+report_label <- paste("The maximum total cost is ", max(lineitems$Tot_Cost),
+			"!!!!", sep="")
